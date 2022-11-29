841872aacc373aa1e48ed1828595a56bf71c5ed2
endritvs <104865231+endritvs@users.noreply.github.com>
2022-11-29 17:23:57 +0100

    Update AllMyFiles.blade.php

--- resources/views/Profile/AllMyFiles.blade.php
+++ resources/views/Profile/AllMyFiles.blade.php
@@ -18,7 +18,7 @@
     <div class="max-w-screen-xl mx-auto pb-6 px-4 sm:px-6 lg:pb-16 lg:px-8">
         <div class="bg-white rounded-lg shadow overflow-hidden">
             <div class="divide-y divide-gray-200 lg:grid lg:grid-cols-12 lg:divide-y-0 lg:divide-x">
-                @include('profile/aside')
+                @include('Profile/aside')
 
                 <div class="divide-y divide-gray-200 lg:col-span-9">
                     <div class="py-6 px-4 sm:p-6 lg:pb-8">
