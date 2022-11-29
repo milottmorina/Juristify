740803a21ce69ce694ea0352682f2994d7af7260
endritvs <104865231+endritvs@users.noreply.github.com>
2022-11-29 18:16:38 +0100

    Update IsAdmin.php

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -17,7 +17,7 @@ class IsAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        if (Auth::user() &&  Auth::user()->role === 1) {
+        if (Auth::user() &&  Auth::user()->role === true) {
             return $next($request);
        }
 
