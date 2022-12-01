48933b3d480a578925907069f049bfa72ee838e7
endritvs <104865231+endritvs@users.noreply.github.com>
2022-12-01 21:33:20 +0100

    Update single.blade.php

--- resources/views/Blog/single.blade.php
+++ resources/views/Blog/single.blade.php
@@ -114,7 +114,7 @@
                   </button>
                   <div class="p-6 text-center">
                       <svg aria-hidden="true" class="mx-auto mb-4 w-14 h-14 text-gray-400 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                      <h3 class="mb-5 text-lg font-normal text-gray-500 ">Are you sure you want to delete this product?</h3>
+                      <h3 class="mb-5 text-lg font-normal text-gray-500 ">Are you sure you want to delete this?</h3>
                       <a href="{{route('comment.delete',$c->id)}}">
                       <button data-modal-toggle="popup-modal{{$c->id}}" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
                           Yes, I'm sure
@@ -132,7 +132,7 @@
                 <!-- Modal header -->
                 <div class="flex justify-between items-start p-4 rounded-t border-b ">
                     <h3 class="text-xl font-semibold text-gray-900 ">
-                        Terms of Service
+                        Edit Comment
                     </h3>
                     <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center " data-modal-toggle="defaultModal{{$c->id}}">
                         <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
@@ -153,9 +153,6 @@
           </div>
           
       </div>
-    
-    <p class="ml-auto text-xs text-gray-500 ">Remember, contributions to this topic should follow our <a href="#" class="text-blue-600  hover:underline">Community Guidelines</a>.</p>
-    
                 </div>
                 <!-- Modal footer -->
                 <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 ">
