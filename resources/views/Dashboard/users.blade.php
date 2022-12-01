407e7c342303b3d01b3aec7e9274c22cf779fbb2
endritvs <104865231+endritvs@users.noreply.github.com>
2022-12-01 20:10:38 +0100

    Update users.blade.php

--- resources/views/Dashboard/users.blade.php
+++ resources/views/Dashboard/users.blade.php
@@ -137,14 +137,14 @@
                                     {{$u->email}}
                                 </td>
                                 <td class="py-4 px-6 overflow-clip uppercase text-center">
-                                    @if($u->verified!=1)
+                                    @if($u->verified!=true)
                                     <svg class="w-5 h-5 text-red-500 text-center" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                                     @else
                                     <svg class="w-5 h-5 text-green-500 text-center" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
                                     @endif
                                 </td>
                                 <td class="py-4 px-6 overflow-clip uppercase">
-                                    @if($u->role!=1)
+                                    @if($u->role!=true)
                                     <p class="bg-red-700 text-white rounded p-1 text-center">USER</p>
                                     @else
                                     <p class="bg-green-700 text-white rounded p-1 text-center">ADMIN</p>
@@ -231,7 +231,7 @@
                                                         </span></div>
                                                 <div class="flex">
                                                 <span>Role:</span><span class="text-sm text-gray-500 uppercase font-bold mt-0.5 ml-1">
-                                                    @if($u->role!=1)
+                                                    @if($u->role!=true)
                                                     <p class="bg-red-700 text-white rounded p-1 text-center">USER</p>
                                                     @else
                                                     <p class="bg-green-700 text-white rounded p-1 text-center">ADMIN</p>
@@ -251,7 +251,7 @@
                                                     
                                                 <div class="flex space-x-3 md:mt-6">
                                                     <a href="#" class="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white rounded-lg">
-                                                        @if ($u->verified!=1)
+                                                        @if ($u->verified!=true)
                                                         <a  data-modal-toggle="popup-modal{{$u->id}}" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ">VERIFY</a>
                                                         @else
                                                         <a data-modal-toggle="popup-modali{{$u->id}}" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-xs px-5 py-2.5 mr-2 mb-2 ">UN-VERIFY</a>
